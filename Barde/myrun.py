12783c3ea97023a3714756b3d3fdb5639480a219
PBarde <paul.b.barde@gmail.com>
2018-01-24 14:24:11 +0100

    transfer sur mon git

--- Barde/myrun.py
+++ Barde/myrun.py
@@ -1,8 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 24 14:11:45 2018
+
+@author: paul
+"""
+
 # You're not allowed to change this file
 from ple.games.flappybird import FlappyBird
 from ple import PLE
 import numpy as np
-from FlappyAgent import FlappyPolicy
+#from FlappyAgent import FlappyPolicy
 
 game = FlappyBird()
 p = PLE(game, fps=30, frame_skip=1, num_steps=1, force_fps=False, display_screen=True)
@@ -19,7 +27,8 @@ for i in range(nb_games):
     while(not p.game_over()):
         state = game.getGameState()
         screen = p.getScreenRGB()
-        action=FlappyPolicy(state, screen) ### Your job is to define this function.
+#        action=FlappyPolicy(state, screen) ### Your job is to define this function.
+        action = None
         
         reward = p.act(action)
         cumulated[i] = cumulated[i] + reward
