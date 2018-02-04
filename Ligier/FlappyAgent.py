9e9c642319d73b26c8f4cccaa6fad755a5bed771
Valentin L <valentin.ligier@supaero.isae.fr>
2018-02-04 16:52:28 +0100

    Work in progress
    Edited the action chosen from predict method, expected 0 or 119 but was 0 or 1

--- Ligier/FlappyAgent.py
+++ Ligier/FlappyAgent.py
@@ -1,25 +1,32 @@
 #import matplotlib.pyplot as plt
 from skimage import transform,color
+import numpy as np
+#from keras.models import Sequential
+#from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
+from keras.models import load_model
 
-buffersize = 2
-bufferTab = [None]*2
-i = 0
+'''deepQnet = Sequential()
+deepQnet.add(Conv2D(filters=16, kernel_size=(8,8), strides=4,
+                        activation="relu", input_shape=(72,101,4)))
+deepQnet.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid',
+                 data_format=None))
+deepQnet.add(Conv2D(filters=32, kernel_size=(4,4), strides=2,
+                        activation="relu"))
+deepQnet.add(Flatten())
+deepQnet.add(Dense(units=256, activation="relu"))
+deepQnet.add(Dense(units=2, activation="linear"))
+deepQnet.compile(optimizer='adam', loss='mean_squared_error')'''
+deepQnet = load_model('model.h5')
 
 def FlappyPolicy(state, screen):
-    global bufferTab
-    global i
-    global buffersize
+    global deepQnet
+    # Crop at 404 px and resize by dividing by 4
+    screen = transform.resize(color.rgb2gray(screen[:,:404,:]),(screen.shape[0]/4,101))
+    frameStack = np.stack([screen]*4,axis=-1)
 
-    #downsize = 80
-    #screen = transform.resize(color.rgb2gray(screen[:,:404,:]),(downsize,downsize)) # Crop at 404 px 
+    a = deepQnet.predict(np.expand_dims(frameStack,axis=0))
 
+    #plt.imshow(screen, cmap='gray')
+    #plt.show()
 
-    if(state['player_y']+60 > state['next_pipe_bottom_y']):
-        bufferTab[i] = 119
-    else:
-        bufferTab[i] = None
-
-    i=(i+1)%buffersize
-    return bufferTab[i]
-    
-    #return None # Should return an action
+    return None # Should return an action
--- Ligier/train.py
+++ Ligier/train.py
@@ -2,18 +2,20 @@ from ple.games.flappybird import FlappyBird
 from ple import PLE
 import numpy as np
 from collections import deque
-import random 
+import random
 from keras.models import load_model
 from challenge_utils import *  # custom functions
 import argparse
+import time
 
 # Parser to use arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--train", help="mode to train the network: scratch or load",
-                    required=False, default="scratch")
-parser.add_argument("-d", "--display", help="display screen or not", required=False, default=True)
-parser.add_argument("-e", "--evaldisp", help="display screen during evaluation", required=False,
-                    default=False)
+                    required=False, default="load")
+parser.add_argument("-d", "--display", help="display screen or not", required=False, default=True,
+                    type=bool)
+parser.add_argument("-s", "--startstep", help="step you want to start from when training an existing model", required=False,
+                    default=0, type=int)
 args=vars(parser.parse_args())
 
 # Define some constants
@@ -24,6 +26,7 @@ gamma = 0.99
 evaluation_period = 10000
 nb_epochs = total_steps // evaluation_period
 epoch=-1
+list_actions = [0, 119]
 
 def MCeval(network, trials, length, gamma):
     scores = np.zeros((trials))
@@ -33,7 +36,7 @@ def MCeval(network, trials, length, gamma):
         stacked_x = deque([screen_x, screen_x, screen_x, screen_x], maxlen=4)
         x = np.stack(stacked_x, axis=-1)
         for t in range(length):  # SHOULD change this with a while loop
-            a = greedy_action(network, x)
+            a = list_actions[greedy_action(network, x)]
             r = clip_reward(p.act(a))
             screen_y = process_screen(p.getScreenRGB())
             scores[i] = scores[i] + gamma**t * r
@@ -60,9 +63,10 @@ else:
     raise ValueError("train argument should be either 'scratch' or 'load'")
 
 # Load the game and initialization
-game = FlappyBird()
+game = FlappyBird(graphics="fixed")
 p = PLE(game, fps=30, frame_skip=1, num_steps=1, force_fps=True,
-        display_screen=True)
+        display_screen=args['display'])
+
 p.init()
 p.reset_game()
 screen_x = process_screen(p.getScreenRGB())
@@ -71,24 +75,31 @@ x = np.stack(stacked_x, axis=-1)
 replay_memory = MemoryBuffer(replay_memory_size, screen_x.shape, (1,))
 
 Xtest = np.array([x])
-scoreQ = np.zeros((nb_epochs))
+#scoreQ = np.zeros((nb_epochs))
 scoreMC = np.zeros((nb_epochs))
 
-for step in range(total_steps):
+start = time.time()
+for step in range(args['startstep'],total_steps): # add parser argument to choose a starting point
     # evaluation
     if(step%evaluation_period == 0 and step>0): #
+        print('{} steps done in {}'.format(evaluation_period, time.time()-start))
+        print('Starting evaluation...')
         deepQnet.save('model.h5')
         epoch += 1
         # evaluation of initial state
-        scoreQ[epoch] = np.mean(deepQnet.predict(Xtest).max(1))
+        # scoreQ[epoch] = np.mean(deepQnet.predict(Xtest).max(1))
         # roll-out evaluation
-        scoreMC[epoch] = MCeval(network=deepQnet, trials=20, length=700, gamma=gamma)
+        scoreMC[epoch] = MCeval(network=deepQnet, trials=20, length=700, gamma=gamma)  # Check this function
         print('Score at step {}: {}'.format(step,scoreMC[epoch]))
+        start = time.time()
     # action selection
-    if np.random.rand() < epsilon(step):
-        a = random.choice([119, 0])  #None
+    # print(epsilon(step))
+    if np.random.rand() < epsilon(step): # ATTENTION epsilon goes back to 1 when resuming
+        # a = random.choice([119, 0])  #None
+        p_random = random.choice([0.5, 0.2, 0.1, 1/15])
+        a = np.random.choice(list_actions,1,p=[p_random, 1-p_random])[0]
     else:
-        a = greedy_action(deepQnet, x)
+        a = list_actions[greedy_action(deepQnet, x)]
     # step
     r = p.act(a)
     screen_y = process_screen(p.getScreenRGB())
