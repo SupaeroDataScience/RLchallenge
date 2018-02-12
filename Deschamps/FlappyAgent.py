eb796425c5518bd17b7460db0b3ff666ecfeaff4
LouisDeschamps <louis.deschamps@supaero.isae.fr>
2018-02-12 11:26:08 +0100

    Mise en forme finale du fichier FlappyAgent.py

--- Deschamps/FlappyAgent.py
+++ Deschamps/FlappyAgent.py
@@ -2,15 +2,6 @@ import numpy as np
 actions = [None, 119]
 Q = np.load('Q_good.npy').item()
 
-
-# def getstate(state):
-    
-#     state_list = list(state.values())
-#     v_pos = state_list[0]-state_list[4]+512 # Différence de position verticale entre l'oiseau et le bas du prochain tuyau
-#                           #+512 (hauteur totale en pixels) pour ne jamais être négatif
-#     h_pos = state_list[2] #Distance de l'oiseau au prochain tuyau
-#     return v_pos, h_pos
-
 def getstate(state):
 	a = list(state.values())
 	v_pos = np.int(np.round((a[0]-a[3]+512)/10))
