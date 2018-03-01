906f791286a9e66482aa7294d7898c53a0adcc3d
Paul Pontisso <35148239+ppontisso@users.noreply.github.com>
2018-03-01 20:47:39 +0100

    Update FlappyAgent.py

--- RandomBird/FlappyAgent.py
+++ RandomBird/FlappyAgent.py
@@ -1,9 +1,16 @@
 import numpy as np
 
 def FlappyPolicy(state, screen):
-    action=None
-    if(np.random.randint(0,2)<1):
-        action=119
+  
+    batchSize = 2
+
+    # load model
+    
+    qval = model.predict(np.array(list(state.values())).reshape(1,8), batch_size=batchSize) 
+    qval_av_action = qval[0]
+    action = (np.argmax(qval_av_action))*119
+    #print("qval = ", qval, "state =" , np.array(list(state.values())).reshape(1,8))
     return action
+
 
 
