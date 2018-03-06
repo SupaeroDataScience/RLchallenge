bd6ae2273572fa8e7403e3dc6716c3759c203318
Paul Pontisso <35148239+ppontisso@users.noreply.github.com>
2018-03-06 10:10:50 +0100

    Update FlappyAgent.py

--- Pontisso/FlappyAgent.py
+++ Pontisso/FlappyAgent.py
@@ -1,16 +1,18 @@
+from keras.models import load_model
 import numpy as np
 
-def FlappyPolicy(state, screen):
-  
-    batchSize = 2
+model = load_model("bestmodel.dqf")
 
-    # load model
+batchSize = 2
+
+
+def FlappyPolicy(state, screen):
     
     qval = model.predict(np.array(list(state.values())).reshape(1,8), batch_size=batchSize) 
     qval_av_action = qval[0]
     action = (np.argmax(qval_av_action))*119
-    #print("qval = ", qval, "state =" , np.array(list(state.values())).reshape(1,8))
     return action
+
 
 
 
