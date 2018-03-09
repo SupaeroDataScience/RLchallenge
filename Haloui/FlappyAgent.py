0ff23e82ce1bba1f7f0b11be6391538c9db1b327
i.h <ihaloui@github.com>
2018-03-09 17:31:48 +0100

    commit_flappy_screen

--- Haloui/FlappyAgent.py
+++ Haloui/FlappyAgent.py
@@ -1,2 +1,27 @@
-def FlappyPolicy(state,screen):
-	return 0
+import numpy as np
+#state: type dict
+#   player_y 
+#   player_vel
+#   next_pipe_dist_to_player
+#   next_pipe_top_y
+#   next_pipe_bottom_y
+#   next_next_pipe_dist_to_player
+#   next_next_pipe_top_y
+#   next_next_pipe_bottom_y
+
+#screen: RGB 
+#   screen: np.array(288,512,3) 
+
+#action: None or 119
+
+#Initial state: writes as state['string']
+    #{'player_y': 256, 'player_vel': 0, 'next_pipe_dist_to_player': 283, 'next_pipe_top_y': 53, 'next_pipe_bottom_y': 153, 'next_next_pipe_dist_to_player': 427.0, 'next_next_pipe_top_y': 153, 'next_next_pipe_bottom_y': 253}
+
+def FlappyPolicy(state, screen,p):
+    action=None
+    print(p.act(action))
+    if(np.random.randint(0,2)<1):
+        action=119
+    return action
+
+
--- Haloui/run.py
+++ Haloui/run.py
@@ -2,10 +2,11 @@
 from ple.games.flappybird import FlappyBird
 from ple import PLE
 import numpy as np
-import FlappyPolicy
+from FlappyAgent import FlappyPolicy
 
-game = FlappyBird()
+game = FlappyBird(graphics="fixed") # use "fancy" for full background, random bird color and random pipe color, use "fixed" (default) for black background and constant bird and pipe colors.
 p = PLE(game, fps=30, frame_skip=1, num_steps=1, force_fps=False, display_screen=True)
+# Note: if you want to see you agent act in real time, set force_fps to False. But don't use this setting for learning, just for display purposes.
 
 p.init()
 reward = 0.0
@@ -23,6 +24,6 @@ for i in range(nb_games):
         
         reward = p.act(action)
         cumulated[i] = cumulated[i] + reward
-
+        
 average_score = np.mean(cumulated)
 max_score = np.max(cumulated)
