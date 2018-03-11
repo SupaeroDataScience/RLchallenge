2bf212c7cc962aa7db1b4fc26beffd9430172241
GuillaumePl <35734948+GuillaumePl@users.noreply.github.com>
2018-03-11 13:09:08 +0100

    Update run.py

--- Plessix/run.py
+++ Plessix/run.py
@@ -6,7 +6,7 @@ import numpy as np
 #from FlappyAgent import FlappyPolicy 
 import FlappyAgent
 
-game = FlappyBird(graphics="fixed") # use "fancy" for full background, random bird color and random pipe color, use "fixed" (default) for black background and constant bird and pipe colors.
+game = FlappyBird() # use "fancy" for full background, random bird color and random pipe color, use "fixed" (default) for black background and constant bird and pipe colors.
 p = PLE(game, fps=30, frame_skip=1, num_steps=1, force_fps=False, display_screen=True)
 # Note: if you want to see you agent act in real time, set force_fps to False. But don't use this setting for learning, just for display purposes.
 
@@ -22,7 +22,7 @@ for i in range(nb_games):
     while(not p.game_over()):
         state = game.getGameState()
         screen = p.getScreenRGB()
-        action=FlappyPolicy(state, screen)
+        action=FlappyAgent.FlappyPolicy(state, screen)
         
         reward = p.act(action)
         cumulated[i] = cumulated[i] + reward
