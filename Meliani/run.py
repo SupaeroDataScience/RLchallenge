ac078d52280694b709e2fad95b39a0bab22931a3
meliani09 <melimostafa@gmail.com>
2018-03-11 20:07:01 +0100

    update

--- Meliani/run.py
+++ Meliani/run.py
@@ -5,7 +5,7 @@ import numpy as np
 from FlappyAgent import FlappyPolicy
 
 game = FlappyBird() # use "fancy" for full background, random bird color and random pipe color, use "fixed" (default) for black background and constant bird and pipe colors.
-p = PLE(game, fps=30, frame_skip=1, num_steps=1, force_fps=True, display_screen=True)
+p = PLE(game, fps=30, frame_skip=1, num_steps=1, force_fps=False, display_screen=True)
 # Note: if you want to see you agent act in real time, set force_fps to False. But don't use this setting for learning, just for display purposes.
 
 p.init()
