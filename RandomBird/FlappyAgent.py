2b1604488182f548b68bb0c2a25b0bb178d596c9
yuanhaorannnnnn <37274406+yuanhaorannnnnn@users.noreply.github.com>
2018-03-11 23:03:34 +0100

    Update FlappyAgent.py

--- RandomBird/FlappyAgent.py
+++ RandomBird/FlappyAgent.py
@@ -1,9 +1,17 @@
 import numpy as np
+Q=np.load("trained_Q.npy")
 
 def FlappyPolicy(state, screen):
-    action=None
-    if(np.random.randint(0,2)<1):
-        action=119
-    return action
 
+    # Using "state"
+    y = int(288 + (state['next_pipe_top_y'] + state['next_pipe_bottom_y']) * 0.5 - state['player_y'])
+    x = int(state['next_pipe_dist_to_player'])
+    v = int(state['player_vel'])
+                
+    action=None
+    action = int(np.argmax(Q[y][x][v][:]))
+    if (action == 1): 
+            action = 119
+          
+    return action
 
